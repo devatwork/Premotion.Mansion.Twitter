fac2cc163d4685d2f5a340a2d0335aee8b338687
Bert Willems <b.willems@premotion.nl>
2013-08-02 12:19:26 +0200

    + changed version number to 0.0.1-alpha

--- build.bat
+++ build.bat
@@ -4,11 +4,6 @@ if "%config%" == "" (
 	set config=Release
 )
 
-set version=
-if not "%PackageVersion%" == "" (
-   set version=-Version %PackageVersion%
-)
-
 set nuget=
 if "%nuget%" == "" (
     set nuget=.nuget\NuGet.exe
@@ -21,17 +16,33 @@ if "%apikey%" == "" (
 
 rem Update self %nuget%
 %nuget% update -self
+if %errorlevel% neq 0 goto failure
 
 rem Set API key
 %nuget% setapikey %apikey% -Source "https://www.myget.org/F/premotion/api/v2/package"
+if %errorlevel% neq 0 goto failure
 %nuget% setapikey %apikey% -Source "https://nuget.symbolsource.org/MyGet/premotion"
+if %errorlevel% neq 0 goto failure
 
 rem Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Premotion.Mansion.Twitter.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if %errorlevel% neq 0 goto failure
 
 rem Package
 %nuget% pack Premotion.Mansion.Twitter\Premotion.Mansion.Twitter.csproj -sym -Prop Configuration=Release
+if %errorlevel% neq 0 goto failure
 
 rem Publish
-%nuget% push Premotion.Mansion.Twitter.0.1.0.nupkg -Source "https://www.myget.org/F/premotion/api/v2/package"
-%nuget% push Premotion.Mansion.Twitter.0.1.0.symbols.nupkg -Source "https://nuget.symbolsource.org/MyGet/premotion"
+%nuget% push Premotion.Mansion.Twitter.0.0.1-alpha.nupkg -Source "https://www.myget.org/F/premotion/api/v2/package"
+if %errorlevel% neq 0 goto failure
+%nuget% push Premotion.Mansion.Twitter.0.0.1-alpha.symbols.nupkg -Source "https://nuget.symbolsource.org/MyGet/premotion"
+if %errorlevel% neq 0 goto failure
+
+:success
+echo success
+goto end
+
+:failure
+echo Failed
+
+:end
